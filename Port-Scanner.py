d6db5e98b51471969f62666b6c268840acb685cd
yoshi-jp <137300301+7654jp@users.noreply.github.com>
2025-05-21 12:28:35 +0900

    Update Port-Scanner.py

--- Port-Scanner.py
+++ Port-Scanner.py
@@ -4,71 +4,42 @@ import threading
 
 from datetime import datetime
 
-import argparse
+def chk_uint(st):
+	try:
+		return int(st)
+	except ValueError:
+		return -1
 
-
-
-# Function to scan a single port
-
-def scan_port(target, port):
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    socket.setdefaulttimeout(1)
-
-    result = s.connect_ex((target, port))
-
-    if result == 0:
-
-        print(f"Port {port}: Open")
-
-    else:
-
-        print(f"Port {port}: Closed")
-
-    s.close()
-
-
-
-# Function to scan a range of ports
-
-def port_scan(target, start_port, end_port):
-
-    target_ip = socket.gethostbyname(target)
-
-    print("-" * 50)
-
-    print(f"Scanning Target: {target_ip}")
-
-    print(f"Scanning started at: {str(datetime.now())}")
-
-    print("-" * 50)
-
-
-
-    for port in range(start_port, end_port + 1):
-
-        thread = threading.Thread(target=scan_port, args=(target_ip, port))
-
-        thread.start()
-
+# Scan 2^11 ports
+def scan_port(tgt, prt):
+	for p in range(prt, prt+2048):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(0.001)
+		result = s.connect_ex((tgt, p))
+		if (result == 0):
+			print(f"Port {p}: Open")
+			s.close()
+			portlist.append(p)
+		else:
+			#print(f"Port {p}: Closed")
+			s.close()
 
 
 # Main
 
 if __name__ == "__main__":
+	global portlist
+	portlist = []
+	print("Python port scanner\nMade by: @MadlyAbi, edited by: @7654jp")
+	target = input("Target IP/Hostname:")
 
-    parser = argparse.ArgumentParser(description="Simple Port Scanner")
+	target_ip = socket.gethostbyname(target)
 
-    parser.add_argument("target", help="Target IP address or hostname")
+	print("-" * 32)
+	print(f"Scanning Target: {target_ip}")
+	print(f"Scanning started at: {str(datetime.now())}")
+	print("-" * 32)
 
-    parser.add_argument("start_port", type=int, help="Start port range")
-
-    parser.add_argument("end_port", type=int, help="End port range")
-
-    args = parser.parse_args()
-
-
-
-    port_scan(args.target, args.start_port, args.end_port)
-
+	for port in range(0, 32):
+		thread = threading.Thread(target=scan_port, args=(target_ip, port * 2048))
+		thread.start()
